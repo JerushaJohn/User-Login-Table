30e7c9e8a8cbed37c0330a70e72f3a9753751cf2
Jerusha John <jerushajd26@gmail.com>
2022-01-19 12:46:47 +0530

    User login details in Table

---
+++ src/component/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+
+        <div className='home' > 
+         <img src='https://blog.hubspot.com/hubfs/homepage-web-design.jpg'/>
+        </div>
+    )
+}
+
+export default Home
